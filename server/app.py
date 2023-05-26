e91c42127e7c34294c21d008ea9bb64375e60b66
Jeffrey Martinez <martinezjf2@gmail.com>
2023-05-25 21:13:10 -0400

    Done: learned about how to create routes and how to render html within flask

--- server/app.py
+++ server/app.py
@@ -4,5 +4,37 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return f'{parameter}'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    count = f''
+    for num in range(parameter):
+        count += f'{num}\n'
+    return count
+    
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '-':
+        return f'{num1 - num2}'
+    elif operation == '+':
+        return f'{num1 + num2}'
+    elif operation == '/':
+        return f'{num1 / num2}'
+    elif operation == '*':
+        return f'{num1 * num2}'
+    elif operation == '%':
+        return f'{num1 % num2}'
+    return '1.0'
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
